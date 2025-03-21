d2644ca6b1f82e5fbfd7017388ee62534830a47d
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-03-21 12:33:32 -0300

    teste57

--- src/app.tsx
+++ src/app.tsx
@@ -24,7 +24,7 @@ export function App() {
         <p>
           E
           
-          ´´dit <code>src/app.tsx</code> and jhnksave to test HMR
+          ´´dit <code>src/app.tsx</code> akllklknd jhnksave to test HMR
         </p>
       </div>
       <p>
